b94ff450dedabc252d330f5c79f089c244e7332d
sher.somas <sher.somas@gmail.com>
2025-02-03 07:34:50 +0530

    functions based code.

--- evaluate_responses.py
+++ evaluate_responses.py
@@ -1,28 +1,47 @@
 import pandas as pd 
 from Evaluator import Evaluator
 import tqdm
-
-llama_responses = pd.read_excel('llama3.2-1b-responses.xlsx', usecols=["prompt", "responses"])
+import os
 
 evaluator = Evaluator(client_type="ollama", model="llama3.1:8b")
 
-result = []
-
-for _, row in tqdm.tqdm(llama_responses.iterrows()):
-    
-    prompt = row['prompt']
-    response = row['responses']
-
-    query = f"""
-            prompt: {prompt} response: {response}"""
-    
-    scores = evaluator.evaluate_all(query)
+responses = ['llama3.2-1b-responses.xlsx', 'deepseek-r1-1.5b-responses.xlsx']
 
 
-    result.append(scores)
+def read_responses(filename):
 
-df = pd.DataFrame(result)
+    responses = pd.read_excel('llama3.2-1b-responses.xlsx', usecols=["prompt", "responses"])
 
-df.to_excel('llama_evaluation.xlsx',index=False)
-df.to_csv('llama-evaluations.csv',index=False)
+    return responses[:100]
 
+def run_evlautions():
+
+    for model_response in responses:
+
+        result = []
+
+        excel_file = read_responses(model_response)
+
+        for _, row in tqdm.tqdm(excel_file.iterrows()):
+            
+            prompt = row['prompt']
+            response = row['responses']
+
+            query = f"""
+                    prompt: {prompt} response: {response}"""
+            
+            scores = evaluator.evaluate_all(query)
+            scores['prompt'] = prompt
+            scores['responses'] = response
+
+            result.append(scores)
+
+        df = pd.DataFrame(result)
+
+        save_name = os.path.join('evaluations', model_response.split('-responses.xlsx')[0] + '-evaluations.xlsx')
+
+        df.to_excel(save_name,index=False)
+        df.to_csv(save_name,index=False)
+
+
+run_evlautions()
